05bcc2e90c1f5dca9e688f06a64daa96e08d15a7
salaahl <67805036+salaahl@users.noreply.github.com>
2023-07-01 11:51:22 +0200

    Update observer.js
    Ajustement du threshold

--- public/js/observer.js
+++ public/js/observer.js
@@ -1,7 +1,7 @@
 const target = ['#about-me h2 span', '.project', '#contact h2 span'];
 
 const options = {
-  threshold: 1,
+  threshold: 0.25,
 };
 
 const observer = new IntersectionObserver(handleIntersection, options);
