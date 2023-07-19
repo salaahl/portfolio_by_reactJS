a9bcc70995fccfb468c27ee2047ff64a6ad160d3
salaahl <67805036+salaahl@users.noreply.github.com>
2023-07-19 17:06:15 +0200

    Update Projects.tsx
    Correction d'un lien

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -91,7 +91,7 @@ function Project(props: ProjectProps) {
 
 Project.defaultProps = {
     title: 'Titre',
-    image: '\images\placeholder.png',
+    image: '/images/placeholder.png',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     github: '#',
     website: '#'
