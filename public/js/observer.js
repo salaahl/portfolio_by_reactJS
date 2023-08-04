8287dab1858eeac0aa0dd00a06c3f77c71e6c4b0
salaahl <67805036+salaahl@users.noreply.github.com>
2023-08-04 14:24:36 +0200

    Update observer.js

--- public/js/observer.js
+++ public/js/observer.js
@@ -27,3 +27,14 @@ window.addEventListener("load", () => {
     }
   });
 });
+
+screen.addEventListener("load", () => {
+  target.forEach((index) => {
+    let container = document.querySelectorAll(index);
+    if (container) {
+      for (let i = 0; i < container.length; i++) {
+        observer.observe(container[i]);
+      }
+    }
+  });
+});
