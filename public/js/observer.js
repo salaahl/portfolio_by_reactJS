a7d6b8af7b413f0b212086a4215fa08e61f0ba51
salaahl <sokhona.salaha@gmail.com>
2024-06-15 19:57:14 +0200

    revert

--- public/js/observer.js
+++ public/js/observer.js
@@ -1,40 +1,32 @@
 const target = ['#about-me h2 span', '.project', '#contact h2 span'];
 
-target.forEach((selector) => {
-  document.querySelectorAll(selector).forEach((ele) => {
-    ele.classList.add('animate');
-  });
-});
-
-if (
-  'IntersectionObserver' in window &&
-  'IntersectionObserverEntry' in window &&
-  'intersectionRatio' in window.IntersectionObserverEntry.prototype
-) {
-  target.forEach((selector) => {
-    document.querySelectorAll(selector).forEach((ele) => {
-      ele.classList.remove('animate');
+if (!'IntersectionObserver' in window &&
+    !'IntersectionObserverEntry' in window &&
+    !'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
+    target.forEach((selector) => {
+      document.querySelectorAll(selector).forEach((ele) => {
+        ele.classList.add('animate');
+      })
     });
-  });
-
+} else {
   const options = {
     threshold: 0.95,
   };
-
+  
   const observer = new IntersectionObserver(handleIntersection, options);
-
+  
   function handleIntersection(entries) {
     entries.map((entry) => {
       function animate() {
         entry.target.classList.add('animate');
       }
       if (entry.isIntersecting) {
-        setTimeout(animate, 250);
+        setTimeout(animate, 250)
       }
     });
   }
-  if (document.documentElement.clientWidth >= 1024) {
-    window.addEventListener('load', () => {
+  if(document.documentElement.clientWidth >= 1024) {
+    window.addEventListener("load", () => {
       target.forEach((index) => {
         let container = document.querySelectorAll(index);
         if (container) {
