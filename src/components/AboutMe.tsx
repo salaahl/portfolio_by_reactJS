6690aa428f10fdd8f6215127ae73a254ddaa2dad
salaahl <sokhona.salaha@gmail.com>
2024-07-09 02:29:00 +0200

    V3

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,7 +7,7 @@ function AboutMe(props: AboutMeProps) {
     return (
         <section id="about-me">
             <div className="content">
-                <h2>{props.title}<span>moi</span></h2>
+                <h2>{props.title}<span>m</span><span>o</span><span>i</span></h2>
                 <p>{props.description}</p>
             </div>
         </section>
@@ -15,8 +15,8 @@ function AboutMe(props: AboutMeProps) {
 }
 
 AboutMe.defaultProps = {
-    title: 'A propos de ',
-    description: 'Bienvenue sur mon portfolio. Je m\'appelle Salaha SOKHONA, j\'ai 29 ans et je suis de Paris. Bibliothécaire passionné par l\'informatique et titulaire d\'un bac + 2 en developpement web, j\'ai décidé de me lancer dans l\'aventure. J\'aime créer des projets innovants et me challenger en permanence. Mon expérience en bibliothèque m\'a appris l\'importance de l\'organisation et de la rigueur, des qualités que je mets à profit dans mon travail de développeur.',
+    title: 'à propos de ',
+    description: 'Bienvenue sur mon portfolio. Je m\'appelle Salaha Sokhona, j\'ai 29 ans et je suis de Paris. Bibliothécaire passionné par l\'informatique et titulaire d\'un DUT informatique, j\'ai décidé de me lancer dans l\'aventure. J\'aime créer des projets innovants et me challenger en permanence. Mon expérience en bibliothèque m\'a appris l\'importance de l\'organisation et de la rigueur, des qualités que je mets à profit dans mon travail de développeur.',
 };
 
 export default AboutMe;
