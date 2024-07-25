718f42d5c2bcdf20b175ad3d77b414c2b48aa79b
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-07-25 15:45:43 +0200

    Update AboutMe.tsx

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -16,7 +16,7 @@ function AboutMe(props: AboutMeProps) {
 
 AboutMe.defaultProps = {
     title: 'à propos de ',
-    description: 'Bienvenue sur mon portfolio. Je m\'appelle Salaha Sokhona, j\'ai 29 ans et je suis de Paris. Bibliothécaire passionné par l\'informatique et titulaire d\'un DUT informatique, j\'ai décidé de me lancer dans l\'aventure. J\'aime créer des projets innovants et me challenger en permanence. Mon expérience en bibliothèque m\'a appris l\'importance de l\'organisation et de la rigueur, des qualités que je mets à profit dans mon travail de développeur.',
+    description: 'Bienvenue sur mon portfolio. Je m\'appelle Salaha Sokhona, j\'ai 30 ans et je suis de Paris. Bibliothécaire passionné par l\'informatique et titulaire d\'un DUT informatique, j\'aime créer des projets innovants et me challenger en permanence. Mon expérience en bibliothèque m\'a appris l\'importance de l\'organisation et de la rigueur, des qualités que je mets à profit dans mon travail de développeur.',
 };
 
 export default AboutMe;
