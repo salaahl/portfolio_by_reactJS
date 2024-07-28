8a90b98704760838fd0698b016ab986e6f5ddcfa
salaahl <sokhona.salaha@gmail.com>
2024-07-28 05:26:23 +0200

    t

--- src/assets/js/observer.js
+++ src/assets/js/observer.js
@@ -1,62 +1,42 @@
-const target = [
-  '#home > svg',
-  '#about-me > .content > p',
-  '#projects > .header',
-  '.project',
-  '#contact > .content',
-];
-
-let options;
-
-if (window.innerWidth < 1024) {
-  options = {
-    root: document.body,
-    threshold: 0.1,
-  };
-} else {
-  options = {
-    root: document.body,
-    threshold: 0.1,
-  };
-}
-
-function handleIntersection(entries) {
-  entries.map((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.style.opacity = 1.0;
-      entry.target.classList.add('animate');
-    }
-  });
-}
-
-const observer = new IntersectionObserver(handleIntersection, options);
-
 window.addEventListener('load', () => {
-  if ('IntersectionObserver' in window) {
-    alert('oui.');
-    console.log(observer)
-
-    // Ajoutez vos éléments à observer ici, par exemple :
-    // observer.observe(document.querySelector('.element-to-observe'));
+  const target = [
+    '#home > svg',
+    '#about-me > .content > p',
+    '#projects > .header',
+    '.project',
+    '#contact > .content',
+  ];
+  
+  let options;
+  
+  if (window.innerWidth < 1024) {
+    options = {
+      root: document.body,
+      threshold: 0.000001
+    };
   } else {
-    // Fallback pour les navigateurs qui ne supportent pas IntersectionObserver
-    alert('IntersectionObserver n’est pas pris en charge par ce navigateur.');
-    // Vous pouvez implémenter un polyfill ou une autre solution ici
+    options = {
+      root: document.body,
+      threshold: 0.000001
+    };
   }
-
+  
+  const observer = new IntersectionObserver(handleIntersection, options);
+  
+  function handleIntersection(entries) {
+    entries.map((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+      }
+    });
+  }
+  
   target.forEach((index) => {
-    let container = document.querySelector(index);
-    observer.observe(container);
-
-
+    let container = document.querySelectorAll(index);
     if (container) {
-
       for (let i = 0; i < container.length; i++) {
         observer.observe(container[i]);
       }
-    } else {
-      alert('non');
-
     }
   });
 });
