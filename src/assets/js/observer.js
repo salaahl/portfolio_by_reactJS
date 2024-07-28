c2419e20709bc392caec383c7cd6e84bf9fe3cd3
salaahl <sokhona.salaha@gmail.com>
2024-07-28 05:13:04 +0200

    t

--- src/assets/js/observer.js
+++ src/assets/js/observer.js
@@ -27,22 +27,22 @@ function handleIntersection(entries) {
   });
 }
 
-let observer;
+const observer = new IntersectionObserver(handleIntersection, options);
 
-if ('IntersectionObserver' in window) {
-  observer = new IntersectionObserver(handleIntersection, options);
-  // Ajoutez vos éléments à observer ici, par exemple :
-  // observer.observe(document.querySelector('.element-to-observe'));
-} else {
-  // Fallback pour les navigateurs qui ne supportent pas IntersectionObserver
-  alert('IntersectionObserver n’est pas pris en charge par ce navigateur.');
-  // Vous pouvez implémenter un polyfill ou une autre solution ici
-}
 window.addEventListener('load', () => {
+  if ('IntersectionObserver' in window) {
+    alert('oui.');
+
+    // Ajoutez vos éléments à observer ici, par exemple :
+    // observer.observe(document.querySelector('.element-to-observe'));
+  } else {
+    // Fallback pour les navigateurs qui ne supportent pas IntersectionObserver
+    alert('IntersectionObserver n’est pas pris en charge par ce navigateur.');
+    // Vous pouvez implémenter un polyfill ou une autre solution ici
+  }
+
   target.forEach((index) => {
     let container = document.querySelector(index);
-
-    console.log(container)
     observer.observe(container);
 
 
