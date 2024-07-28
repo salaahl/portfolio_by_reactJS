5f374c69cc5271e6eaa19dd7c50d1d8d6534e1ad
salaahl <sokhona.salaha@gmail.com>
2024-07-28 06:12:02 +0200

    t

--- src/assets/js/observer.js
+++ src/assets/js/observer.js
@@ -1,44 +1,34 @@
-window.addEventListener('load', () => {
-  const target = [
-    '#home > svg',
-    '#about-me > .content > p',
-    '#projects > .header',
-    '.project',
-    '#contact > .content',
-  ];
+if (window.innerWidth > 1023) {
+  window.addEventListener('load', () => {
+    const target = [
+      '#home > svg',
+      '#about-me > .content > p',
+      '#projects > .header',
+      '.project',
+      '#contact > .content',
+    ];
 
-  let options;
-
-  if (window.innerWidth < 1024) {
-    options = {
-      root: document.querySelector('body'),
-      threshold: 0.5,
-    };
-  } else {
-    options = {
-      root: document.querySelector('body'),
+    let options = {
       threshold: 1,
     };
-  }
 
-  function handleIntersection(entries) {
-    entries.map((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.style.opacity = 1;
+    const observer = new IntersectionObserver(handleIntersection, options);
+
+    function handleIntersection(entries) {
+      entries.map((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+        }
+      });
+    }
+
+    target.forEach((index) => {
+      let container = document.querySelectorAll(index);
+      if (container) {
+        for (let i = 0; i < container.length; i++) {
+          observer.observe(container[i]);
+        }
       }
     });
-  }
-
-  const observer = new IntersectionObserver(handleIntersection, options);
-
-  observer.root.style.border = "10px solid #44aa44";
-
-  target.forEach((index) => {
-    let container = document.querySelectorAll(index);
-    if (container) {
-      for (let i = 0; i < container.length; i++) {
-        observer.observe(container[i]);
-      }
-    }
   });
-});
+}
