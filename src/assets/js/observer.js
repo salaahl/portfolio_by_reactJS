8ca63b3006467f0d0ae5502b0465d2885d969118
salaahl <sokhona.salaha@gmail.com>
2024-08-13 02:19:07 +0200

    optimisation de l'observer

--- src/assets/js/observer.js
+++ src/assets/js/observer.js
@@ -1,98 +1,66 @@
-// or all tools are exported from the "all" file (excluding members-only plugins):
 import { gsap, ScrollTrigger } from 'gsap/all';
 import SplitType from 'split-type';
 
-// don't forget to register plugins
+// Enregistrement des plugins
 gsap.registerPlugin(ScrollTrigger);
 
+// Configuration globale de GSAP
 gsap.config({
-  trialWarn: true, // Affiche un avertissement dans la console si j'utilise un plug-in payant
+  trialWarn: true, // Affiche un avertissement si un plug-in payant est utilisé
 });
 gsap.defaults({ ease: 'power1.out' });
-let mm = gsap.matchMedia();
 
+// Fonction pour changer le fond d'écran
+function changeBackground(color) {
+  gsap.to('#root', {
+    backgroundColor: color,
+    duration: 0, // Peut être ajusté si nécessaire
+    ease: 'power1.out',
+  });
+}
+
+// Création des animations ScrollTrigger
 window.addEventListener('load', () => {
-  /* animation : fond d'écran */
-  gsap.to('#root', {
-    backgroundColor: 'rgba(0, 0, 0, 1)',
-
-    scrollTrigger: {
-      trigger: '#home',
-      start: '0% 100%',
-      end: '100% 100%',
-      scrub: true,
-    },
+  // Animation de fond d'écran
+  ScrollTrigger.create({
+    trigger: '#projects > .header',
+    start: '100% 100%',
+    end: '100% 100%',
+    onEnter: () => changeBackground('rgb(255, 255, 255)'),
+    onEnterBack: () => changeBackground('rgb(0, 0, 0)'),
   });
 
-  gsap.to('#root', {
-    backgroundColor: 'rgba(255, 255, 255, 1)',
-
-    scrollTrigger: {
-      trigger: '#projects > .header',
-      start: '100% 100%',
-      end: '100% 100%',
-      scrub: true,
-    },
+  ScrollTrigger.create({
+    trigger: '#contact',
+    start: '100% 100%',
+    end: '100% 100%',
+    onEnter: () => changeBackground('rgb(103, 175, 142)'),
+    onEnterBack: () => changeBackground('rgb(255, 255, 255)'),
   });
 
-  gsap.to('#root', {
-    backgroundColor: 'rgba(255, 255, 255, 1)',
-
-    scrollTrigger: {
-      trigger: '#projects > .header',
-      start: '100% 100%',
-      end: '100% 100%',
-      scrub: true,
-    },
-  });
-
-  gsap.from('#root', {
-    backgroundColor: 'rgba(255, 255, 255, 1)',
-
-    scrollTrigger: {
-      trigger: '#contact',
-      start: '100% 0%',
-      end: '100% 0%',
-      scrub: true,
-    },
-  });
-
-  gsap.to('#root', {
-    backgroundColor: 'rgba(103, 175, 142, 1)',
-
-    scrollTrigger: {
-      trigger: '#contact',
-      start: '100% 100%',
-      end: '100% 100%',
-      scrub: true,
-    },
-  });
-
-  /* animation : fadeIn */
-  let target = [
+  // Animation des cibles
+  const targets = [
     { name: '#home', start: '15% 50%' },
     { name: '#about-me', start: '15% 50%' },
     { name: '#projects > .header', start: '100% 100%' },
     { name: '#contact', start: '15% 50%' },
   ];
 
-  for (let i = 0; i < target.length; i++) {
-    gsap.from(target[i].name + '> *', {
+  targets.forEach(({ name, start }) => {
+    gsap.from(`${name} > *`, {
       opacity: 0,
       y: '25%',
       duration: 0.5,
-
       scrollTrigger: {
-        trigger: target[i].name,
-        start: target[i].start,
+        trigger: name,
+        start,
       },
     });
-  }
+  });
 
-  /* animation : arrow */
+  // Animation de la flèche
   gsap.from('#home > .arrow', {
     opacity: 1,
-
     scrollTrigger: {
       trigger: '#home',
       start: '0%',
@@ -101,45 +69,31 @@ window.addEventListener('load', () => {
     },
   });
 
-  /* animation isolée : paragraphe de la section "apropos" */
-  // 1er paramètre : la div à découper
-  let typeSplit = new SplitType('#about-me p', {
-    types: 'lines, words, chars', // words : mots, "lines" : lignes et "chars" : caractères. Les 3 sont combinables comme ici
-    tagName: 'span', // les mots seront mis dans des balises span
+  // Animation du paragraphe dans la section "à propos"
+  new SplitType('#about-me p', {
+    types: 'lines, words, chars',
+    tagName: 'span',
   });
 
   gsap.from('#about-me p .word', {
-    opacity: 0.1, // l'opacité de départ de la balise span
-    duration: 0.5, // la duree de l'animation
-    ease: 'power1.out', // l'effet de l'animation
-    stagger: 0.1, // équivaut à un animation-delay, va mettre 0.1 entre chaque mot
-
+    opacity: 0.1,
+    duration: 0.5,
+    ease: 'power1.out',
+    stagger: 0.1,
     scrollTrigger: {
-      trigger: '#about-me', // la div à surveiller pour lancer l'animation
-      start: window.innerWidth < 1024 ? '50% 75%' : '15% 50%', // la zone de la div SURVEILLEE à partir de laquelle mon animation commence ET la position du curseur dans la fenêtre
+      trigger: '#about-me',
+      start: window.innerWidth < 1024 ? '50% 75%' : '15% 50%',
       end: window.innerWidth < 1024 ? '75% 75%' : '50% 50%',
-      scrub: true, // l'animation suit le scroll
+      scrub: true,
     },
   });
 
-  /* animation isolée : mes projets */
+  // Animation de la taille du fond du h2
   gsap.fromTo(
     '#projects > .header > h2',
-    {
-      backgroundSize: '0% 100%',
-      stagger: 0.1,
-
-      scrollTrigger: {
-        trigger: '#projects > .header',
-        start: '100% 100%',
-        end: '0% 0%',
-        scrub: true,
-      },
-    },
+    { backgroundSize: '0% 100%' },
     {
       backgroundSize: '100% 100%',
-      stagger: 0.1,
-
       scrollTrigger: {
         trigger: '#projects > .header',
         start: '100% 100%',
@@ -149,8 +103,8 @@ window.addEventListener('load', () => {
     }
   );
 
-  /* animation isolée : mes projets */
-  mm.add('(max-width: 1023px)', () => {
+  // Animation pour les projets en fonction de la taille de l'écran
+  gsap.matchMedia().add('(max-width: 1023px)', () => {
     const projects = gsap.utils.toArray('#projects .project');
     projects.forEach((project) => {
       gsap.from(project, {
@@ -158,24 +112,21 @@ window.addEventListener('load', () => {
         y: '25%',
         opacity: 0,
         duration: 0.5,
-
         scrollTrigger: {
-          trigger: project,
-          start: '0% 75%',
-          end: '100% 75%',
+          trigger: project.querySelector('img'),
+          start: '50% 100%',
         },
       });
     });
   });
 
-  mm.add('(min-width: 1024px)', () => {
+  gsap.matchMedia().add('(min-width: 1024px)', () => {
     gsap.from('#projects .project', {
       pointerEvents: 'none',
       y: '25%',
       opacity: 0,
       duration: 0.5,
       stagger: 0.1,
-
       scrollTrigger: {
         trigger: '#projects > .header',
         start: '0% 0%',
@@ -183,23 +134,10 @@ window.addEventListener('load', () => {
     });
   });
 
-  /* animation isolée : contact */
-  gsap.from('#contact h2 span:first-child', {
+  // Animation des spans dans la section contact
+  gsap.from('#contact h2 span', {
     opacity: 0,
-    x: '-100%',
-
-    scrollTrigger: {
-      trigger: '#contact',
-      start: '0% 50%',
-      end: '50% 50%',
-      scrub: true,
-    },
-  });
-
-  gsap.from('#contact h2 span:last-child', {
-    opacity: 0,
-    x: '100%',
-
+    x: (index) => (index === 0 ? '-100%' : '100%'),
     scrollTrigger: {
       trigger: '#contact',
       start: '0% 50%',
