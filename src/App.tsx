9c3946e7c69165039c854732dfcd85c739b6b0e7
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-08-17 12:42:35 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <ul>
             <li><a href="#home">Accueil</a></li>
             <li><a href="#about-me">A propos de moi</a></li>
-            <li><a href="#projects">Projets</a></li>
+            <li><a href="#projects-anchor">Projets</a></li>
             <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
