f371b0c20df70693b8a5a1a727ab41f092ce7829
salaahl <sokhona.salaha@gmail.com>
2025-07-12 00:42:42 +0200

    refactor: mise a jour de la description

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -10,8 +10,7 @@ function AboutMe() {
         </h2>
         <p>
           Bienvenue sur mon portfolio. Je m'appelle Salaha, j'ai 31 ans et je
-          suis de Paris. Bibliothécaire passionné par l'informatique et
-          titulaire d'un DUT informatique, j'aime créer des projets innovants et
+          suis de Paris. Bibliothécaire passionné par l'informatique, j'aime créer des projets innovants et
           me challenger en permanence. Mon expérience en bibliothèque m'a appris
           l'importance de l'organisation et de la rigueur, des qualités que je
           mets à profit dans mon travail de développeur.
